8d936f472df81b0ae2d85401759ff0cca603bcab
nehakeshava <keshava.neha@gmail.com>
2020-02-05 22:27:38 +0530

    first commit

---
+++ src/main/java/git_pkg/git_proj/App.java
@@ -0,0 +1,13 @@
+package git_pkg.git_proj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
